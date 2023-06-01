ea105af7bb2ba257ea255794397fd6cf438b404e
Tran Huu Binh <tranhuubinh0198@gmail.com>
2023-06-02 00:49:57 +0700

    update source code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <ul class="box">
+      <li class="item1"></li>
+      <li class="item2"></li>
+    </ul>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.box {
+  .item1 {
+    width: 200px;
+    height: 200px;
+    background-color: aqua;
+  }
+
+  .item2 {
+    width: 200px;
+    height: 200px;
+    background-color: bisque;
+  }
+}
+</style>
